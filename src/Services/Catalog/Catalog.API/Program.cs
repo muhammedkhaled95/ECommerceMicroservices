4602cd3c81b54ae5ac0c58298e33378ea9c63401
muhammedkhaled95 <muhammedkhaled1110@gmail.com>
2025-03-27 04:24:08 +0200

    Catalog: Clean up the program file for the catalog service project.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add all the services to the DI container.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
 
 app.Run();
