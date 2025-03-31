eb251c5c35a6c3a0970ac2002c896c0b9a56840a
muhammedkhaled95 <muhammedkhaled1110@gmail.com>
2025-03-31 06:02:51 +0200

    Using ICommandHandler and ICommand in the CreateProductHandler Class instead of IRequestHandler and IRequest.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,17 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
 // Records are an excellent choice for CQRS Command, Query, and Result objects
 // because they are primarily data carriers and often immutable
 public record CreateProductCommand(string Name, List<string> Category, string Description,
-                                   string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+                                   string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
