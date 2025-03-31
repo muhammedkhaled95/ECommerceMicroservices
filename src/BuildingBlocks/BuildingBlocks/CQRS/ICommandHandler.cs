d844ff5183382692bb82b45c4dbe2d7df931ce2f
muhammedkhaled95 <muhammedkhaled1110@gmail.com>
2025-03-31 05:59:38 +0200

    Catalog: Adding ICommandHandler and IQueryHandler where both inherit from IRequestHandler.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+/// <summary>
+/// Defines a handler for a command that does not return any data.
+/// </summary>
+/// <typeparam name="TCommand">
+/// The type of the command to handle. The command must implement <see cref="ICommand{Unit}"/>.
+/// </typeparam>
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+/// <summary>
+/// Defines a handler for a command that returns a result of type <typeparamref name="TResponse"/>.
+/// </summary>
+/// <typeparam name="TCommand">
+/// The type of the command to handle. The command must implement <see cref="ICommand{TResponse}"/>.
+/// </typeparam>
+/// <typeparam name="TResponse">
+/// The type of the response returned after handling the command.
+/// Must be a non-nullable type.
+/// </typeparam>
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
+
