f422676fafcb93af71d949f3244d70f37abf1dfe
muhammedkhaled95 <muhammedkhaled1110@gmail.com>
2025-03-31 04:21:02 +0200

    Catalog:
    feat(CQRS): Add ICommand and IQuery interfaces for command and query separation
    
    - Introduced `ICommand<TResponse>` and `ICommand` interfaces for CQRS commands.
    - Added `IQuery<TResponse>` interface for query handling.
    - Provided XML documentation for better maintainability.
    - Ensured a clean separation between commands and queries using MediatR.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+
+/// <summary>
+/// Represents a command in the CQRS pattern that performs an action and does not return a value.
+/// </summary>
+public interface ICommand : ICommand<Unit>
+{
+}
+
+/// <summary>
+/// Represents a command in the CQRS pattern that performs an action and returns a result.
+/// </summary>
+/// <typeparam name="TResponse">
+/// The type of the response returned by the command.
+/// </typeparam>
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
+
+
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+/// <summary>
+/// Represents a query in the CQRS pattern that retrieves data without modifying state.
+/// </summary>
+/// <typeparam name="TResponse">
+/// The type of the response returned by the query. Must be a non-nullable type.
+/// </typeparam>
+public interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull
+{
+}
