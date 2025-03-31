457c22b5708201e13cf7f578ab61df8a2da81549
muhammedkhaled95 <muhammedkhaled1110@gmail.com>
2025-03-31 03:00:13 +0200

    Catalog: Creating the boiler plate for the first command in CQRS pattern (CreateProduct).

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+// Records are an excellent choice for CQRS Command, Query, and Result objects
+// because they are primarily data carriers and often immutable
+public record CreateProductCommand(string Name, List<string> Category, string Description,
+                                   string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
